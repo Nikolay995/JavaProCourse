c3a218e8eb541a3df4a6a041431595091795ae80
Nikolay_K <striker.kms@gmail.com>
2018-05-24 10:55:42 +0300

    DecimalFormat added

--- src/xml_json/json_parsing/currencies/Main.java
+++ src/xml_json/json_parsing/currencies/Main.java
@@ -1,28 +1,35 @@
 package xml_json.json_parsing.currencies;
 
 import com.google.gson.Gson;
-
-import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.text.DecimalFormat;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         String request = "http://data.fixer.io/api/latest?access_key=8fc4616c91275ba5511e19bf7ed2853a&format=1";
         String response = "";
-
+        String curr;
+        double val;
         Gson gson = new Gson();
-        Currency currency = null;
-
+        Currency currency ;
         try {
             response = Service.performRequest(request);
-            currency = gson.fromJson(response, Currency.class);
+            currency= gson.fromJson(response, Currency.class);
             Double baseCurrency = currency.getRates().get("UAH");
-            String inputCurrency = Service.readInput();
-            Double inputValue = currency.getRates().get(inputCurrency);
 
-            System.out.println(baseCurrency/inputValue);
-        } catch (FileNotFoundException e) {
-            e.printStackTrace();
+            System.out.print("Введите нужную валюту (EUR/USD/...): ");
+            curr = new Scanner(System.in).next().toUpperCase();
+
+            System.out.print("Введите сумму: ");
+            Double inputValue = currency.getRates().get(curr);
+
+            val = Double.parseDouble(new Scanner(System.in).next());
+            Double res = val * (baseCurrency / inputValue);
+
+            DecimalFormat df = new DecimalFormat("#.00");
+            System.out.println("\n"+val + " " + curr + " = "+ df.format(res) + " UAH");
+
         } catch (IOException e) {
             e.printStackTrace();
         }
--- src/xml_json/json_parsing/currencies/Service.java
+++ src/xml_json/json_parsing/currencies/Service.java
@@ -1,18 +1,15 @@
 package xml_json.json_parsing.currencies;
 
-import com.sun.org.apache.bcel.internal.generic.RETURN;
-
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
-import java.util.Scanner;
 
-public class Service {
+class Service {
 
      static String performRequest(String urlAddress) throws IOException {
-        String result ="";
+        StringBuilder result = new StringBuilder();
         try {
             URL url = new URL(urlAddress);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -20,21 +17,14 @@ public class Service {
             try(BufferedReader br = new BufferedReader(new InputStreamReader(connection.getInputStream()))) {
                 String temp = "";
                 for (; (temp = br.readLine()) != null;) {
-                    result+=temp;
-                    result+=System.lineSeparator();
+                    result.append(temp);
+                    result.append(System.lineSeparator());
                 }
             }
         } catch (IOException e) {
-            throw e;
+            e.printStackTrace();
         }
-        return result;
+        return result.toString();
     }
 
-    static String readInput(){
-        return new Scanner(System.in).next();
-    }
-
-    static boolean isValidInput(String input){
-         return true;
-    }
 }
